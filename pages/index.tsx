f2b8cd187c895d092c008dbb0509c23ef23a8dbf
Brian-Patterson <pbpatterson92@gmail.com>
2023-01-17 18:34:17 -0800

    added some global css

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,27 +9,10 @@ import { ResponseCookies } from 'next/dist/server/web/spec-extension/cookies'
 const inter = Inter({ subsets: ['latin'] })
 
 export const getStaticProps: GetStaticProps = async () => {
-  // const [peopleRes, postsRes] = await Promise.all([
-  //   fetch('https://fakerapi.it/api/v1/persons?_quantity=10'),
-  //   fetch('https://fakerapi.it/api/v1/texts?_quantity=1&_characters=500')
-  // ])
-  // const responses: (Person | Post)[] = await Promise.all([
-  //   peopleRes.json(),
-  //   String(postsRes.json())
-  // ])
-  // console.log(responses)
-
-  // return {
-  //   props: {
-  //     responses
-  //   }
-  // }
-
   const res = await fetch('https://fakerapi.it/api/v1/persons?_quantity=10')
   const peopleData : Person = await res.json()
   
-
-  const res2 = await fetch('https://fakerapi.it/api/v1/texts?_quantity=10&_characters=500')
+  const res2 = await fetch('https://fakerapi.it/api/v1/texts?_quantity=10&_characters=50')
   const postData : PostInfo = await res2.json()
 
   return {
@@ -41,7 +24,7 @@ export const getStaticProps: GetStaticProps = async () => {
 }
 
 
-const Home: NextPage<{peopleData : Person, postData: PostInfo, responses: Person[] | Post[]}> = ({peopleData, postData, responses}) => {
+const Home: NextPage<{peopleData : Person, postData: PostInfo}> = ({peopleData, postData}) => {
   return (
     <>
       <Head>
@@ -50,29 +33,31 @@ const Home: NextPage<{peopleData : Person, postData: PostInfo, responses: Person
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* <div>
-        {responses.map((response) => {
-          return (
-          <p>{response}</p>
-        )})}
-      </div> */}
-      <ul>
+      <header className='main-header'>
+        <h3 className='title-header'>Social Mockida</h3>
+        <section className='userOptions'>
+        <h4 className='login-header'>Login</h4>
+        <h4 className='signup-header'>Sign Up</h4>
+        </section>
+      </header>
+      <ul className='main-content'>
         {peopleData.data.map((person) => {
           return (
-            <div key={person.id}>
-              <li>{person.firstname} {person.lastname}</li>
-              <Image 
+            <div key={person.id} className='content-items'>
+              <Image
+                className='content-image' 
                 unoptimized
                 src={person.image}
                 alt={person.firstname}
-                width="200"
-                height="200"
+                width="50"
+                height="50"
                 />
-        {postData.data.map((post) => {
-          return (
-            <p>{post.content}</p>
-            )
-          })}
+              <h5 className='content-name'>{person.firstname} {person.lastname}</h5>
+            {postData.data.map((post, id) => {
+              return (
+                <p key={id} className='content-post'>{post.content}</p>
+                )
+              })}
             </div>
           )
         })}
